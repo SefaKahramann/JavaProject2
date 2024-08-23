b7cab69ef8e06796e163f9a2067d3e770c18a19c
AbdulkerimMutlu <a.kerim1990@gmail.com>
2024-08-23 23:59:33 +0300

    deleteCalısanWithId metodu yazıldı

--- src/Employee_Project/Veritabani/Calisanlar.java
+++ src/Employee_Project/Veritabani/Calisanlar.java
@@ -6,20 +6,18 @@ import java.util.ArrayList;
 public class Calisanlar {
 
     private static ArrayList<Calisan> calisanList=new ArrayList<>();
-
     public static ArrayList<Calisan> getCalisanList() {
         return calisanList;
     }
-
-
     public static void addACalisan(Calisan calisan) {
         calisanList.add(calisan);
     }
-
     public static void deleteACalisanWithId(String calisanId) {
-
-
-
+        for (Calisan e:calisanList)
+            if (e.equals(calisanId)){
+                calisanList.remove(e);
+            }else
+                System.out.println("ID Bulunamadı");
     }
 
     public static void printDepartmandakiCalisanlar(String departmanKodu) {
