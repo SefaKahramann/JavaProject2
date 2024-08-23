a85cf692ba06c0aca80c14627fdbc18a309cd929
SefaKahramannn <sefakahraman47ak@gmail.com>
2024-08-23 20:52:42 +0300

    setDepartman dolduruldu hatalar düzeltildi calışanlara hashmap eklendi

--- src/Employee_Project/Veritabani/Calisanlar.java
+++ src/Employee_Project/Veritabani/Calisanlar.java
@@ -1,30 +1,24 @@
 package Employee_Project.Veritabani;
 
 import Employee_Project.Modeller.Calisanlar.Calisan;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Calisanlar {
 
+    private static Map<String,String > calisanList=new HashMap<>();
 
-    // Buradaki calisanList static cünkü proje calismaya basladiği anda oluşması lazım.
-    // Bunu bir veritabani gibi görebiliriz. Calisanlarimizin hepsi bu liste icerisinde yer alacak.
-    private static ArrayList<Calisan> calisanList = new ArrayList<>();
-
-    // // Tüm çalışanları almak için
-    public static ArrayList<Calisan> getCalisanList() {
+    public static Map<String,String> getCalisanList() {
         return calisanList;
     }
 
 
     public static void addACalisan(Calisan calisan) {
-
-        // TODO Bir çalışan eklemek için gerekli method. addACalisan() methodunu doldurunuz
-
-
+        calisanList.put(calisan.getCalisanId(),calisan);
     }
 
-
     public static void deleteACalisanWithId(String calisanId) {
+
 
         // TODO Bir çalışan silmek için gerekli method. deleteACalisanWithId() methodunu doldurunuz
 
