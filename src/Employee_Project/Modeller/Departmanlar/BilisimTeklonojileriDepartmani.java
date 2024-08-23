6c56a76d8035d0b32edbee91cdbe3a487bb7d856
SefaKahramannn <sefakahraman47ak@gmail.com>
2024-08-23 18:34:27 +0300

    Dosyalar ayarlandı Düzenlemeler yapılacaktır

---
+++ src/Employee_Project/Modeller/Departmanlar/BilisimTeklonojileriDepartmani.java
@@ -0,0 +1,21 @@
+package Employee_Project.Modeller.Departmanlar;
+
+public class BilisimTeklonojileriDepartmani implements Departman {
+
+    final String departmanKodu = "BTD";
+
+    // TODO Bu class'da sadece zam oranini belirtmelisiniz
+    //      Başka bişeyi değiştirmeye gerek yok.
+    final int zamOrani = 0;
+
+
+    @Override
+    public int getZamOrani() {
+        return zamOrani;
+    }
+
+    @Override
+    public String getDepartmanKodu() {
+        return departmanKodu;
+    }
+}
